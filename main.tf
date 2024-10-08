d622ad6caabed54d231a5b6af3087baf13c6aa78
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-10-08 19:03:25 +0200

    infra: initialise and setup basic docker and main.tf

---
+++ main.tf
@@ -0,0 +1,150 @@
+variable "azs" {
+  type        = list(string)
+  description = "Availability Zones"
+  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Demo VPC"
+  }
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "Demo VPC IG"
+  }
+}
+
+resource "aws_route_table" "second_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.second_rt.id
+}
+
+resource "aws_security_group" "demo-sg" {
+  name_prefix = "demo-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecr_repository" "demo-repo" {
+  name = "Demo Repo"
+}
+
+# resource "aws_ecr_lifecycle_policy" "demo-lifecycle-policy" {
+#   repository = aws_ecr_repository.demo-repo
+#   policy     = <<EOF
+#   {
+#     "rules": [
+#         {
+#             "rulePriority": 1,
+#             "description": "Expire images older than 14 days",
+#             "selection": {
+#                 "tagStatus": "untagged",
+#                 "countType": "sinceImagePushed",
+#                 "countUnit": "days",
+#                 "countNumber": 14
+#             },
+#             "action": {
+#                 "type": "expire"
+#             }
+#         }
+#     ]
+# }
+# EOF
+# }
+
+resource "aws_instance" "demo-sg" {
+  ami           = "ami-006dcf34c09e50022" // Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    aws_security_group.demo-sg.id,
+  ]
+}
+
+output "public_ip" {
+  value = aws_instance.demo-sg.public_ip
+}
